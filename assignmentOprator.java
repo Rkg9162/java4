8f9d317aff2b8d35f737b7a988d1d86a4f48e205
Rakesh Kumar <rakesh18146@gmail.com>
2024-04-27 17:46:12 +0530

    java4 start

---
+++ assignmentOprator.java
@@ -0,0 +1,7 @@
+public class assignmentOprator {
+    public static void main(String[] args) {
+        int a,b,c,d;
+        a=b=c=d=10;
+        System.out.println(a+ " + "+ b+ "+"+c+ "+"+ d);
+    }
+}
