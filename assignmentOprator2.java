3f28e0cae3e819710182a5281034b17a54e28255
Rakesh Kumar <rakesh18146@gmail.com>
2024-04-27 17:59:10 +0530

    assignmentOprator

--- assignmentOprator2.java
+++ assignmentOprator2.java
@@ -1,11 +1,11 @@
 public class assignmentOprator2 {
     public static void main(String[] args) {
-        int a,b,c,d;
-        a=b=c=d=10;
-       a= a+=10;
-      b=  a-=10;
-        c=a*=10;
-        d=a/=10;
+        int a, b, c, d;
+        a = b = c = d = 10;
+        a = a += 10;
+        b = a -= 10;
+        c = a *= 10;
+        d = a /= 10;
 
         System.out.println(a);
         System.out.println(b);
@@ -13,7 +13,7 @@ public class assignmentOprator2 {
         System.out.println(d);
 
     }
-    
+
 }
 
-//output = 20
+// output = 20
