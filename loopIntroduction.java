e75b97bb0a6603cb209a334098446c3355593b1e
Rakesh Kumar <rakesh18146@gmail.com>
2024-04-30 22:07:48 +0530

    Introduction of loop

---
+++ loopIntroduction.java
@@ -0,0 +1,10 @@
+public class loopIntroduction {
+    public static void main(String[] args) {
+
+        for(int i =0; i<5; i++)
+        {
+            System.out.println("*");
+        }
+    }
+}
+//Introduction of loop
