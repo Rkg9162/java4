320803495b0f241a433e1c0b75bea5d55d3c1cac
Rakesh Kumar <rakesh18146@gmail.com>
2024-04-30 22:13:37 +0530

    5 star print using while loop

---
+++ whileLoop.java
@@ -0,0 +1,11 @@
+public class whileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            System.out.println("*");
+            i++;
+
+        }
+    }
+}
+//print 5 star usin while loop
