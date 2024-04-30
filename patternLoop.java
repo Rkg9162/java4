6687f30da5a972c0fc47d3913ea99017520e249e
Rakesh Kumar <rakesh18146@gmail.com>
2024-04-30 22:29:15 +0530

    pattern programming using while loop

---
+++ patternLoop.java
@@ -0,0 +1,16 @@
+public class patternLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            int k = 0;
+            while (k < 5) {
+                System.out.print("* ");
+                k++;
+            }
+            System.out.println();
+            i++;
+
+        }
+    }
+//pattern programming using while loop
+}
