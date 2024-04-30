b1665ce872ba63c51604d11f44dbd9b5bce64587
Rakesh Kumar <rakesh18146@gmail.com>
2024-04-30 21:58:06 +0530

    switchCase

---
+++ switchCase.java
@@ -0,0 +1,19 @@
+public class switchCase {
+    private static final String Rakesh = null;
+
+    public static void main(String[] args) {
+        String Name = "Rakesh";
+        switch(Name)
+        {
+            case "Rakesh" : System.out.println("this is true name ");
+            break;
+            case "Rahul" : System.out.println("this is false name ");
+            break;
+            case "amit" : System.out.println("this is false name ");
+            break;
+        }
+    }
+    
+}
+// break  keyword use for break line for match case condition 
+// if i am not using break keyword so all below statement be print when case is match
